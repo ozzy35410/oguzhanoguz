837812cf28bdb3a32df7493c73bcbbf5f40becef
Oguzhan Oguz <gsoguzhanoguz@gmail.com>
2025-07-02 03:18:47 +0300

    Change About page photo to use about.jpg

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   XIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/foto.jpg'
+import portraitImage from '@/images/about.jpg'
 
 function SocialLink({
   className,
